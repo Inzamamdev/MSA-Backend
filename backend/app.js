64f40e68e4d360a6743651998ca57fc6b0bfe46c
Inzamam Shaikh <92295764+456535688765@users.noreply.github.com>
2025-03-02 05:53:11 +0530

    cors added

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import searchRoutes from "./src/routes/searchRoutes.js";
-
+import cors from "cors";
+dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.CLIENT_API_URL,
+  })
+);
 app.use(bodyParser.json());
 app.use("/search", searchRoutes);
 
